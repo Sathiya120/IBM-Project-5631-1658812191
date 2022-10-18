2e7089cb35b52c23c7c6a07da25d5e353efdee59
Niranjan1220 <115540711+Niranjan1220@users.noreply.github.com>
2022-10-18 18:46:59 +0530

    Delete ASSIGNMENT-2 directory

--- ASSIGNMENT-2/schema.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS client_details;
-CREATE TABLE client_details (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT NOT NULL,
-  email TEXT NOT NULL,
-  password TEXT NOT NULL
-);
